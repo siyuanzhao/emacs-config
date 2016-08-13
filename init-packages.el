2692de08079f30ee1ac600c0e3bd40634981ed16
Siyuan Zhao <siyuan.vince.zhao@gmail.com>
2016-08-13 12:56:47 -0400

    add new packages and remove old ones

--- init-packages.el
+++ init-packages.el
@@ -8,7 +8,7 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.org/packages/")))
 ; list the packages you want
-(setq package-list '(ac-math better-defaults smartparens switch-window golden-ratio helm  magit tangotango-theme jedi swiper auto-complete paradox ein request websocket easy-kill undo-tree latex-preview-pane  color-theme-solarized ace-jump-mode elpy))
+(setq package-list '(better-defaults smartparens switch-window golden-ratio jedi-core company company-jedi helm magit swiper paradox ein request websocket easy-kill undo-tree latex-preview-pane solarized-theme ace-jump-mode elpy auctex company-math))
 
 
 ; activate all the packages (in particular autoloads)
